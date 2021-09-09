dbf87957671e6af1864ae9c8f4f0d09796f72bed
Emanuel Benetti <emanuelbenetti39@gmail.com>
2021-09-09 20:23:58 -0300

    Agregada capa de manzanas!

--- map.py
+++ map.py
@@ -1,7 +1,29 @@
 import folium
 
-# Create map object
-m = folium.Map(location=[-32.6859,-58.8910], zoom_start=16)
+# define cual es la pagina donde estan alojados los archivos json de datos
 
-# Generate map
-m.save('map.html')
+url = (
+#    "https://emabenetti.github.io/Urdi-Map"
+)
+cuadras = f"cuadras.geojson"
+
+# Esto crea un objeto mapa con las coordenadas y el zoom que le indiquemos
+# Para este proyecto uso las coordenadas aproximadas del parque de la ciudad y un zoom de 16
+
+m = folium.Map(
+    location=[-32.6859,-58.8910],
+    tiles="cartodbpositron",
+    zoom_start=15,
+)
+
+folium.GeoJson(cuadras, name="Manzanas").add_to(m)
+
+
+folium.LayerControl().add_to(m)
+
+
+
+m
+
+# Esta funcion genera el archivo html con todo lo necesario para mostrar el mapa en el navegador
+m.save('index.html')
