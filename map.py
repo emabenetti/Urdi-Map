636dddf9739fdb8c378b60539c792c77058a9e37
Emanuel Benetti <emanuelbenetti39@gmail.com>
2021-09-09 21:19:21 -0300

    OpenStreetMap y comentarios
    - Agregada la posibilidad de elegir Open Street Map como motor de mapas en el menú de capas
    
    - Agregados comentarios para todas las lineas en map.py
    
    - Se volvio a crear index.html desde map.py

--- map.py
+++ map.py
@@ -1,29 +1,26 @@
 import folium
 
-# define cual es la pagina donde estan alojados los archivos json de datos
-
-url = (
-#    "https://emabenetti.github.io/Urdi-Map"
-)
+# define donde esta el archivo .geojson y lo guarda en una variable
 cuadras = f"cuadras.geojson"
 
 # Esto crea un objeto mapa con las coordenadas y el zoom que le indiquemos
 # Para este proyecto uso las coordenadas aproximadas del parque de la ciudad y un zoom de 16
-
+# tiles se usa para elegir el motor de mapa predeterminado. En la doc de Folium estan todos los disponibles
 m = folium.Map(
     location=[-32.6859,-58.8910],
     tiles="cartodbpositron",
     zoom_start=15,
 )
 
+# Folium convierte el GeoJson a un objeto y lo agrega al mapa
+# name="" es el nombre que aparece en el menu de capas en la web
 folium.GeoJson(cuadras, name="Manzanas").add_to(m)
 
+# Agrego como opcion usar Open Street Map en vez de CartoDB Positron
+folium.TileLayer('openstreetmap').add_to(m)
 
+# Se crea el grupo de controles de capa y se agrega al mapa
 folium.LayerControl().add_to(m)
-
-
-
-m
 
 # Esta funcion genera el archivo html con todo lo necesario para mostrar el mapa en el navegador
 m.save('index.html')
